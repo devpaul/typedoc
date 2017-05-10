194026f8ad2688f4f3fbc2eccc3b716992245f01
Travis <support@sitepen.com>
2017-05-09 19:57:43 -0700

    0.6.0-pre w/ TypeScript 2.3.2

---
+++ dist/lib/models/types/abstract.d.ts
@@ -0,0 +1,10 @@
+export declare abstract class Type {
+    isArray: boolean;
+    readonly type: string;
+    abstract clone(): Type;
+    equals(type: Type): boolean;
+    toObject(): any;
+    toString(): string;
+    static isTypeListSimiliar(a: Type[], b: Type[]): boolean;
+    static isTypeListEqual(a: Type[], b: Type[]): boolean;
+}
